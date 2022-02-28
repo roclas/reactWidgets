6d93e69e38892f52d74d60d946043708ea551ba2
Carlos <carlopolo@gmail.com>
2022-02-28 15:40:49 +0000

    first draft

---
+++ dist/HR.js
@@ -0,0 +1,2 @@
+import React from 'react';
+export const HR = () => /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("hr", null), "hello", /*#__PURE__*/React.createElement("hr", null));
---
+++ src/HR.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const HR=()=><div><hr/>hello<hr/></div>
